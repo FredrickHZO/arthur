51258f770b660a33277ad52a049eb71119316bbc
FredrickHZO <federico.armagno97@gmail.com>
2023-08-02 20:17:45 +0200

    created lexer test

---
+++ lexer/lexer_test.go
@@ -0,0 +1,41 @@
+package lexer
+
+import (
+	"arthur/token"
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := "=+(){},;\n"
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.WHITESPACE, "\n"},
+	}
+
+	l := NewLexer(input)
+
+	for i, item := range tests {
+		tkn := l.NextToken()
+
+		if tkn.Type != item.expectedType {
+			t.Fatalf("tests[%d] - wrong token type, expected=%q, got=%q",
+				i, item.expectedType, tkn.Type)
+		}
+
+		if tkn.Literal != item.expectedLiteral {
+			t.Fatalf("tests[%d] - wrong literal type, expected=%q, got =%q",
+				i, item.expectedLiteral, tkn.Literal)
+		}
+	}
+}
