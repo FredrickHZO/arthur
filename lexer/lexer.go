173217e7dabc0a9326e7315ab165e43866f44e76
FredrickHZO <federico.armagno97@gmail.com>
2023-08-02 19:53:18 +0200

    added token types

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,1 +1,22 @@
 package lexer
+
+// might change to int or byte in the future
+type TokenType string
+
+const (
+	ILLEGAL    = "ILLEGAL"
+	EOF        = "EOF"
+	IDENT      = "IDENT"
+	INT        = "INT"
+	ASSIGN     = "="
+	PLUS       = "+"
+	COMMA      = ","
+	SEMICOLON  = ";"
+	LPAREN     = "("
+	RPAREN     = ")"
+	LBRACE     = "{"
+	RBRACE     = "}"
+	WHITESPACE = "\n"
+	FUNCTION   = "FUNCTION"
+	LET        = "LET"
+)
