27c673e9e3bb4fd3f2aa7bbac2e2433bb0d522d0
FredrickHZO <federico.armagno97@gmail.com>
2023-08-03 21:42:49 +0200

    keywords lookup

--- token/token.go
+++ token/token.go
@@ -8,6 +8,7 @@ type Token struct {
 	Literal string
 }
 
+// TODO: add floats and other symbols
 const (
 	ILLEGAL   = "ILLEGAL"
 	EOF       = "EOF"
@@ -21,7 +22,27 @@ const (
 	RPAREN    = ")"
 	LBRACE    = "{"
 	RBRACE    = "}"
-	NEWLINE   = "\n"
+	NEWLINE   = "NEWLINE"
 	FUNCTION  = "FUNCTION"
 	LET       = "LET"
 )
+
+// this table is useful to discern between user-defined
+// identifiers and keywords
+var keywords = map[string]TokenType{
+	"let": LET,
+	"fn":  FUNCTION,
+	"\n":  NEWLINE,
+}
+
+// checks the table to see if the identifier is
+// a keyword of the language or a user-defined one.
+// If it's a keyword, it returns its TokenType constant
+// else we use IDENT which is the TokenType for
+// all user-defined identifiers
+func LookupIdent(ident string) TokenType {
+	if item, ok := keywords[ident]; ok {
+		return item
+	}
+	return IDENT
+}
