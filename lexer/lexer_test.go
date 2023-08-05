2846b5372686809d33624dc5a0e7038c11c566ae
FredrickHZO <federico.armagno97@gmail.com>
2023-08-05 21:47:43 +0200

    Test with new tokens.

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -2,6 +2,7 @@ package lexer
 
 import (
 	"arthur/token"
+	"fmt"
 	"testing"
 )
 
@@ -13,7 +14,16 @@ func TestNextToken(t *testing.T) {
 			x + y
 		}
 
-		let result = add(five, ten)`
+		let result = add(five, ten)
+		
+		!-/*5
+		5 < 10 > 5
+		
+		if 5 < 10 {
+			return true
+		} else {
+			return false
+		}`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -60,12 +70,49 @@ func TestNextToken(t *testing.T) {
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.NEWLINE, "\n"},
+		{token.NEWLINE, "\n"},
+
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.NEWLINE, "\n"},
+
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RT, ">"},
+		{token.INT, "5"},
+		{token.NEWLINE, "\n"},
+		{token.NEWLINE, "\n"},
+
+		{token.IF, "if"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.LBRACE, "{"},
+		{token.NEWLINE, "\n"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.NEWLINE, "\n"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.NEWLINE, "\n"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.NEWLINE, "\n"},
+		{token.RBRACE, "}"},
 	}
 
 	l := NewLexer(input)
 
 	for i, item := range tests {
 		tkn := l.NextToken()
+
+		fmt.Print(tkn.Literal)
 
 		if tkn.Type != item.expectedType {
 			t.Fatalf("tests[%d] - wrong token type, expected=%q, got=%q",
