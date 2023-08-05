d2ba59cc12bfe75f8c37635482f9ca4d7185df6c
FredrickHZO <federico.armagno97@gmail.com>
2023-08-05 08:54:37 +0200

    Initial unicode support.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,14 +2,14 @@ package lexer
 
 import (
 	"arthur/token"
+	"unicode"
 )
 
-// TODO: change lexer to support unicode
 type Lexer struct {
 	input        string
 	position     int
 	readPosition int
-	char         byte
+	char         rune
 }
 
 func NewLexer(input string) *Lexer {
@@ -21,47 +21,33 @@ func NewLexer(input string) *Lexer {
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.char = 0
-	} else {
-		l.char = l.input[l.readPosition]
+		return
 	}
+	l.char = rune(l.input[l.readPosition])
 	l.position = l.readPosition
 	l.readPosition += 1
 }
 
-func newToken(tokenType token.TokenType, char byte) token.Token {
+func newToken(tokenType token.TokenType, char rune) token.Token {
 	return token.Token{Type: tokenType, Literal: string(char)}
 }
 
-// returns true if the character being read is a letter
-func isLetter(char byte) bool {
-	return 'a' <= char && char <= 'z' ||
-		'A' <= char && char <= 'Z' ||
-		char == '_' ||
-		char == '!' ||
-		char == '\\'
-}
-
-// completely reads keywords and user-defined identifiers
-func (l *Lexer) readIdentifier() string {
+// reads keywords and user-defined identifiers
+func (l *Lexer) lexIdentifier() string {
 	position := l.position
-	for isLetter(l.char) {
+	for unicode.IsLetter(l.char) {
 		l.readChar()
 	}
 	return l.input[position:l.position]
 }
 
-// returns true if the character being read is a digit
-func isDigit(char byte) bool {
-	return '0' <= char && char <= '9'
-}
-
-// completely reads a number while it doesn't find any digit anymore
-func (l *Lexer) readNumber() string {
-	position := l.position
-	for isDigit(l.char) {
+// reads a number until there are no digit left
+func (l *Lexer) lexNumber() string {
+	startPos := l.position
+	for unicode.IsDigit(l.char) {
 		l.readChar()
 	}
-	return l.input[position:l.position]
+	return l.input[startPos:l.position]
 }
 
 // doesn't skip newline since it replaces semicolons
@@ -71,7 +57,6 @@ func (l *Lexer) skipWhiteSpace() {
 	}
 }
 
-// returns the correct token
 func (l *Lexer) NextToken() token.Token {
 	var item token.Token
 
@@ -100,12 +85,14 @@ func (l *Lexer) NextToken() token.Token {
 		item.Literal = ""
 		item.Type = token.EOF
 	default:
-		if isLetter(l.char) {
-			item.Literal = l.readIdentifier()
+		// might break for _ ! \
+		if unicode.IsLetter(l.char) {
+			item.Literal = l.lexIdentifier()
 			item.Type = token.LookupIdent(item.Literal)
 			return item
-		} else if isDigit(l.char) {
-			item.Literal = l.readNumber()
+		}
+		if unicode.IsDigit(l.char) {
+			item.Literal = l.lexNumber()
 			item.Type = token.INT
 			return item
 		}
