21869b83392d95d0740b5bce60a5d927cb6f7fff
FredrickHZO <federico.armagno97@gmail.com>
2023-08-06 11:23:12 +0200

    Update lexer test.

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -2,13 +2,11 @@ package lexer
 
 import (
 	"arthur/token"
-	"fmt"
 	"testing"
 )
 
 func TestNextToken(t *testing.T) {
 	input := `let five = 5
-		let ten = 10
 
 		let add = fn(x, y) {
 			x + y
@@ -16,13 +14,11 @@ func TestNextToken(t *testing.T) {
 
 		let result = add(five, ten)
 		
-		!-/*5
-		5 < 10 > 5
-		
-		if 5 < 10 {
-			return true
-		} else {
+		if !(10 != 9) {
 			return false
+		}
+		if 9 == 9 {
+			return true
 		}`
 
 	tests := []struct {
@@ -33,12 +29,6 @@ func TestNextToken(t *testing.T) {
 		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
 		{token.INT, "5"},
-		{token.NEWLINE, "\n"},
-
-		{token.LET, "let"},
-		{token.IDENT, "ten"},
-		{token.ASSIGN, "="},
-		{token.INT, "10"},
 		{token.NEWLINE, "\n"},
 		{token.NEWLINE, "\n"},
 
@@ -73,36 +63,28 @@ func TestNextToken(t *testing.T) {
 		{token.NEWLINE, "\n"},
 		{token.NEWLINE, "\n"},
 
-		{token.BANG, "!"},
-		{token.MINUS, "-"},
-		{token.SLASH, "/"},
-		{token.ASTERISK, "*"},
-		{token.INT, "5"},
-		{token.NEWLINE, "\n"},
-
-		{token.INT, "5"},
-		{token.LT, "<"},
-		{token.INT, "10"},
-		{token.RT, ">"},
-		{token.INT, "5"},
-		{token.NEWLINE, "\n"},
-		{token.NEWLINE, "\n"},
-
 		{token.IF, "if"},
-		{token.INT, "5"},
-		{token.LT, "<"},
+		{token.BANG, "!"},
+		{token.LPAREN, "("},
 		{token.INT, "10"},
-		{token.LBRACE, "{"},
-		{token.NEWLINE, "\n"},
-		{token.RETURN, "return"},
-		{token.TRUE, "true"},
-		{token.NEWLINE, "\n"},
-		{token.RBRACE, "}"},
-		{token.ELSE, "else"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
 		{token.NEWLINE, "\n"},
 		{token.RETURN, "return"},
 		{token.FALSE, "false"},
+		{token.NEWLINE, "\n"},
+		{token.RBRACE, "}"},
+		{token.NEWLINE, "\n"},
+		{token.IF, "if"},
+		{token.INT, "9"},
+		{token.EQ, "=="},
+		{token.INT, "9"},
+		{token.LBRACE, "{"},
+		{token.NEWLINE, "\n"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
 		{token.NEWLINE, "\n"},
 		{token.RBRACE, "}"},
 	}
@@ -111,8 +93,6 @@ func TestNextToken(t *testing.T) {
 
 	for i, item := range tests {
 		tkn := l.NextToken()
-
-		fmt.Print(tkn.Literal)
 
 		if tkn.Type != item.expectedType {
 			t.Fatalf("tests[%d] - wrong token type, expected=%q, got=%q",
