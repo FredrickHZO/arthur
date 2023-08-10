0fbe2bbddcd157b62df029bc67eaf6bed8381c7c
FredrickHZO <federico.armagno97@gmail.com>
2023-08-10 22:50:59 +0200

    Removed reference to old lexer.

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -2,78 +2,36 @@ package lexer
 
 import (
 	"arthur/token"
-	"fmt"
 	"testing"
 )
 
 func TestNextToken(t *testing.T) {
-	input := `let num = 5.54
-
-		let add = fn(x, y) {
-			x + y
-		}
-
-		let result = add(five, five)
-		pippo`
+	input := `foo
+		let`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.IDENT, "foo"},
+		{token.NEWLINE, "\n"},
 		{token.LET, "let"},
-		{token.IDENT, "num"},
-		{token.ASSIGN, "="},
-		{token.FLOAT, "5.54"},
-		{token.NEWLINE, "\n"},
-		{token.NEWLINE, "\n"},
-
-		{token.LET, "let"},
-		{token.IDENT, "add"},
-		{token.ASSIGN, "="},
-		{token.FUNCTION, "fn"},
-		{token.LPAREN, "("},
-		{token.IDENT, "x"},
-		{token.COMMA, ","},
-		{token.IDENT, "y"},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.NEWLINE, "\n"},
-		{token.IDENT, "x"},
-		{token.PLUS, "+"},
-		{token.IDENT, "y"},
-		{token.NEWLINE, "\n"},
-		{token.RBRACE, "}"},
-		{token.NEWLINE, "\n"},
-		{token.NEWLINE, "\n"},
-
-		{token.LET, "let"},
-		{token.IDENT, "result"},
-		{token.ASSIGN, "="},
-		{token.IDENT, "add"},
-		{token.LPAREN, "("},
-		{token.IDENT, "five"},
-		{token.COMMA, ","},
-		{token.IDENT, "five"},
-		{token.RPAREN, ")"},
-		{token.NEWLINE, "\n"},
-		{token.IDENT, "pippo"},
+		{token.EOF, ""},
 	}
 
-	l := NewLexer(input)
+	items := Lex(input)
 
 	for i, item := range tests {
-		tkn := l.Tokenize()
+		token := <-items
 
-		fmt.Println(tkn)
-
-		if tkn.Type != item.expectedType {
+		if token.Type != item.expectedType {
 			t.Fatalf("tests[%d] - wrong token type, expected=%q, got=%q",
-				i, item.expectedType, tkn.Type)
+				i, item.expectedType, token.Type)
 		}
 
-		if tkn.Literal != item.expectedLiteral {
+		if token.Literal != item.expectedLiteral {
 			t.Fatalf("tests[%d] - wrong literal type, expected=%q, got =%q",
-				i, item.expectedLiteral, tkn.Literal)
+				i, item.expectedLiteral, token.Literal)
 		}
 	}
 }
