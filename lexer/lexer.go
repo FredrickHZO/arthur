1deff4cf6deb742042b3bf6cbfb81603f902e808
FredrickHZO <federico.armagno97@gmail.com>
2023-08-10 22:50:34 +0200

    Partially implemented new lexer.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,211 +2,137 @@ package lexer
 
 import (
 	"arthur/token"
+	"strings"
 	"unicode"
+	"unicode/utf8"
 )
 
-type Lexer struct {
-	in   string
-	pos  int
-	read int
-	char rune
+type lexer struct {
+	items chan token.Token
+	input string
+	start int
+	pos   int
+	width int
 }
+
+type stateFn func(*lexer) stateFn
 
 const EOF = -1
 
-// creates a new lexer with the input string and the first character
-func NewLexer(input string) *Lexer {
-	return &Lexer{
-		in:   input,
-		pos:  0,
-		read: 0,
-		char: rune(input[0]),
+// initializes the lexer and starts to lex the input string
+func Lex(in string) chan token.Token {
+	l := &lexer{
+		input: in,
+		items: make(chan token.Token),
+	}
+	go l.run()
+	return l.items
+}
+
+// effectively starts lexing the input string
+func (l *lexer) run() {
+	for state := lexExpr; state != nil; {
+		state = state(l)
 	}
 }
 
-// return the next item in the input string of the one being lexed
-func (l *Lexer) peek() rune {
-	if l.read >= len(l.in) {
-		return EOF
+// emit passes an item back to the client
+func (l *lexer) emit(t token.TokenType) {
+	l.items <- token.Token{
+		Type:    t,
+		Literal: l.input[l.start:l.pos],
 	}
-	return rune(l.in[l.read])
+	l.start = l.pos
+}
+
+// returns the next char
+// doesn't advance in the input string
+func (l *lexer) peek() rune {
+	r := l.next()
+	l.backup()
+	return r
 }
 
 // advances one position in the input string
-func (l *Lexer) next() {
-	if l.read >= len(l.in) {
-		l.char = EOF
-		return
+func (l *lexer) next() rune {
+	var r rune
+	if l.pos >= len(l.input) {
+		l.width = 0
+		return EOF
 	}
-	l.char = rune(l.in[l.read])
-	l.pos = l.read
-	l.read += 1
+	r, l.width = utf8.DecodeRuneInString(l.input[l.pos:])
+	l.pos += l.width
+	return r
 }
 
 // goes back one position in the input string
-func (l *Lexer) backup() {
-	l.read = l.pos
-	l.pos -= 1
-	l.char = rune(l.in[l.read])
+func (l *lexer) backup() {
+	l.pos -= l.width
 }
 
-// reads keywords and user-defined identifiers
-func (l *Lexer) lexIdentifier() string {
-	start := l.pos
-	for isLetter(l.peek()) {
-		l.next()
-	}
-	str := l.in[start:l.read]
-	return str
+// skips over the pending input before this point
+func (l *lexer) ignore() {
+	l.start = l.pos
 }
 
-func (l *Lexer) consumeDigits() {
-	for isDigit(l.char) {
-		l.next()
+// consumes the next rune if it's from a valid set
+func (l *lexer) accept(set string) bool {
+	if strings.IndexRune(set, l.next()) >= 0 {
+		return true
 	}
+	l.backup()
+	return false
 }
 
-// generates the correct type of token for numbers
-func (l *Lexer) lexNumber() token.Token {
-	start := l.pos
-	var t token.TokenType = token.INT
-	l.consumeDigits()
-
-	// checks if it is a float
-	if l.char == '.' && !isDigit(l.peek()) {
-		l.backup()
-	} else if l.char == '.' && isDigit(l.peek()) {
-		t = token.FLOAT
-		l.next()
-		l.consumeDigits()
+// consumes a run of runes from the valid set
+func (l *lexer) acceptRun(set string) {
+	for strings.IndexRune(set, l.next()) >= 0 {
+		// goes forward until it finds an invalid item
 	}
-	return token.Token{Literal: l.in[start:l.read], Type: t}
+	l.backup()
 }
 
-// generates the correct token for the item being lexed
-func (l *Lexer) Tokenize() token.Token {
-	l.next()
-
-	for isSpace(l.char) {
-		l.next()
-	}
-
-	switch l.char {
-	// operator cases
-	case '=':
-		if l.peek() == '=' {
-			return l.twoCharToken(token.EQ)
-		} else {
-			return l.token(token.ASSIGN)
-		}
-	case '+':
-		next := l.peek()
-		var t token.Token
-		if next == '+' {
-			t = l.twoCharToken(token.INCREMENT)
-		} else if next == '=' {
-			t = l.twoCharToken(token.PLUS_EQ)
-		} else {
-			t = l.token(token.PLUS)
-		}
-		return t
-	case '-':
-		next := l.peek()
-		var t token.Token
-		if next == '-' {
-			t = l.twoCharToken(token.DECREMENT)
-		} else if next == '=' {
-			t = l.twoCharToken(token.MINUS_EQ)
-		} else {
-			t = l.token(token.MINUS)
-		}
-		return t
-	case '!':
-		if l.peek() == '=' {
-			return l.twoCharToken(token.NOT_EQ)
-		} else {
-			return l.token(token.BANG)
-		}
-	case '*':
-		if l.peek() == '=' {
-			return l.twoCharToken(token.ASTERISK_EQ)
-		} else {
-			return l.token(token.ASTERISK)
-		}
-	case '/':
-		if l.peek() == '=' {
-			return l.twoCharToken(token.SLASH_EQ)
-		} else {
-			return l.token(token.SLASH)
-		}
-	case '<':
-		return l.token(token.LT)
-	case '>':
-		return l.token(token.RT)
-	// delimiter cases
-	case ',':
-		return l.token(token.COMMA)
-	case ';':
-		return l.token(token.SEMICOLON)
-	case '(':
-		return l.token(token.LPAREN)
-	case ')':
-		return l.token(token.RPAREN)
-	case '{':
-		return l.token(token.LBRACE)
-	case '}':
-		return l.token(token.RBRACE)
-	case '\n':
-		return l.token(token.NEWLINE)
-	// no more item to lex
-	case EOF:
-		return token.Token{
-			Literal: "",
-			Type:    token.EOF,
-		}
-	// numbers, identifiers, keywords
-	default:
-		if isLetter(l.char) {
-			ident := l.lexIdentifier()
-			return token.Token{
-				Literal: ident,
-				Type:    token.LookupIdent(ident),
-			}
-		}
-		if isDigit(l.char) {
-			return l.lexNumber()
-		}
-		return l.token(token.ILLEGAL)
-	}
-}
-
-// doesn't skip newline since it replaces semicolons
+// newline character must not be recognized as space
 func isSpace(r rune) bool {
 	return r == ' ' || r == '\t' || r == '\r'
 }
 
+// returns the current range from start to pos as a string
+func (l *lexer) current() string {
+	return l.input[l.start:l.pos]
+}
+
+// generates a token for a keyword or user-defined identifier
+func lexIdentifier(l *lexer) stateFn {
+	digits := "abcdefghilmnopqrstuvzkjyxABCDEFGHILMNOPQRSTUVZKJYX123456789_"
+	l.acceptRun(digits)
+	l.emit(token.LookupIdent(l.current()))
+	return lexExpr
+}
+
+// returns true if the current character is a letter
 func isLetter(r rune) bool {
 	return unicode.IsLetter(r) || r == '_'
 }
 
-func isDigit(r rune) bool {
-	return unicode.IsNumber(r)
-}
+// generates the correct token for the item being lexed
+func lexExpr(l *lexer) stateFn {
+	r := l.next()
 
-// returns a single character token
-func (l *Lexer) token(tt token.TokenType) token.Token {
-	return token.Token{
-		Type:    tt,
-		Literal: string(l.in[l.pos]),
-	}
-}
+	switch {
+	case r == EOF:
+		l.emit(token.EOF)
+		return nil
 
-// returns a two-character token
-func (l *Lexer) twoCharToken(tt token.TokenType) token.Token {
-	item := token.Token{
-		Type:    tt,
-		Literal: l.in[l.pos : l.read+1],
+	case r == '\n':
+		l.emit(token.NEWLINE)
+
+	case isSpace(r):
+		l.ignore()
+
+	case isLetter(r):
+		return lexIdentifier
 	}
-	l.next()
-	return item
+
+	return lexExpr
 }
